a59303e23ceb3d4d7bc7e6a7da646ee9846a3b62
Mohd Khairi Mohd Adnan <khairi@labs.my>
2018-05-04 18:30:28 +0800

    update ui

---
+++ lib/shards/ui/engine.rb
@@ -0,0 +1,15 @@
+require 'autoprefixer-rails'
+
+module Shards
+  module Ui
+    module Rails
+      class Engine < ::Rails::Engine
+        initializer 'shards-ui.assets' do |app|
+          %w(stylesheets javascripts).each do |sub|
+            app.config.assets.paths << root.join('assets', sub).to_s
+          end
+        end
+      end
+    end
+  end
+end
